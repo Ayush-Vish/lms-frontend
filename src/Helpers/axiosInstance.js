d6d85948e9fa7340d12a028ffea6d8039bb40e4f
Ayush Vishwakarma <ayushvish6555@gmail.com>
2023-11-16 11:16:37 +0530

    Moved .env in the root directory

--- src/Helpers/axiosInstance.js
+++ src/Helpers/axiosInstance.js
@@ -1,6 +1,9 @@
 import axios from "axios"
 
-const BASE_URL  = "http://localhost:5000/api/v1" 
+const BASE_URL  = import.meta.env.VITE_API_BACKEND_URL   
+
+
+
 
 const axiosInstance = axios.create( ) 
 
@@ -8,4 +11,4 @@ axiosInstance.defaults.baseURL =BASE_URL
 axiosInstance.defaults.withCredentials = true 
 
 
-export default axiosInstance; 
+export default axiosInstance;
